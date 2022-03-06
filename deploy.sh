52756577fe361994d6be20012aee04b03d75cb46
sl798970799 <798970799@qq.com>
2022-03-07 00:31:22 +0800

    feat: 初始化

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果你要部署在 https://<USERNAME>.github.io
-git push -f git@github.com:lin-data-center/vite.git main
+git push -f git@github.com:lin-data-center/vite.git master:gh-pages
 
 # 如果你要部署在 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
