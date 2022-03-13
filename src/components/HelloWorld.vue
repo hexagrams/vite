ae0748c04dbd1629f142a9da32ad8008ab93324b
sl798970799 <798970799@qq.com>
2022-03-13 11:59:21 +0800

    feat: 哈哈

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,7 +26,7 @@ const count = ref(0);
 
     <button type="button" @click="count++">count is: {{ count }}</button>
     <p>
-      Edit
+      Edit2112
       <code>components/HelloWorld.vue</code> to test hot module replacement.
     </p>
   </div>
